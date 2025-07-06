e356681fdfca0e0939a9e14ae4ebe0407939b2e8
GUOGUOGUODING <137122411+GUOGUOGUODING@users.noreply.github.com>
2025-07-06 18:10:14 -0400

    Add files via upload
    0706 Update

---
+++ scripts/jhu-dVRK/draw.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the data from two text files
+file1 = '/home/pshao7/dvrk_python_devel/dataset/MTML_internal_torque_0704.txt'
+file2 = '/home/pshao7/dvrk_python_devel/dataset/MTML_total_torque_0704.txt'
+
+predicted_force = '/home/pshao7/dvrk_python_devel/dataset/MTML_cartesian_internal_force_0704.txt'
+measured_force = '/home/pshao7/dvrk_python_devel/dataset/MTML_cartesian_total_force_0704.txt'
+
+# Load assuming space-separated values
+data1 = np.loadtxt(file1, usecols=(0, 1, 2, 3, 4, 5))
+data2 = np.loadtxt(file2)
+
+# Compute the difference
+diff = data1 - data2
+rmse = np.sqrt(np.mean(diff**2, axis=0))
+max = np.max(data2, axis = 0)
+min = np.min(data2, axis=0)
+
+nrmse = 100 * rmse/(max-min)
+print(nrmse)
+# Plotting
+joint_labels = [f'Joint{i+1}' for i in range(data1.shape[1])]
+timesteps = np.arange(data1.shape[0])
+
+fig, axs = plt.subplots(data1.shape[1], 1, figsize=(10, 2 * data1.shape[1]), sharex=True)
+fig.suptitle('Joint Angles and Their Differences', fontsize=16)
+
+for i in range(data1.shape[1]):
+    axs[i].plot(timesteps, data1[:, i], label='predicted', color='blue')
+    axs[i].plot(timesteps, data2[:, i], label='measured', color='orange')
+    #axs[i].plot(timesteps, diff[:, i], label='Difference', color='green', linestyle='--')
+    axs[i].set_ylabel(joint_labels[i])
+    axs[i].legend(loc='upper right')
+    axs[i].grid(True)
+
+axs[-1].set_xlabel('Timestep')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+#plt.show()
+
+
+# Load assuming space-separated values
+data3 = np.loadtxt(predicted_force, usecols=(0, 1, 2))
+data4 = np.loadtxt(measured_force, usecols=(0, 1, 2))
+
+# Compute the difference
+diff = data3 - data4
+rmse = np.sqrt(np.mean(diff**2, axis=0))
+max = np.max(data4, axis = 0)
+min = np.min(data4, axis=0)
+
+nrmse = 100 * rmse/(max-min)
+print(nrmse)
+# Plotting
+joint_labels = [f'Axis{i+1}' for i in range(data3.shape[1])]
+timesteps = np.arange(data3.shape[0])
+
+fig, axs = plt.subplots(data3.shape[1], 1, figsize=(10, 2 * data3.shape[1]), sharex=True)
+fig.suptitle('Cartisian force', fontsize=16)
+
+for i in range(data3.shape[1]):
+    axs[i].plot(timesteps, data3[:, i], label='predicted', color='blue')
+    axs[i].plot(timesteps, data4[:, i], label='measured', color='orange')
+    #axs[i].plot(timesteps, diff[:, i], label='Difference', color='green', linestyle='--')
+    axs[i].set_ylabel(joint_labels[i])
+    axs[i].legend(loc='upper right')
+    axs[i].grid(True)
+
+axs[-1].set_xlabel('Timestep')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+plt.show()
--- scripts/jhu-dVRK/plot-Multi.py
+++ scripts/jhu-dVRK/plot-Multi.py
@@ -2,80 +2,87 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 
-true_pos = np.loadtxt('multi_array_0605.txt', skiprows=1)
-expect_pos = np.loadtxt('multi_array_exp_0605.txt', skiprows=1)
-error = np.abs(true_pos - expect_pos) * 1000
+file_path = '0704'
+true_pos = np.loadtxt(f'/home/pshao7/dvrk_python_devel/dataset/multi_array.txt', skiprows=1)
+expect_pos = np.loadtxt(f'/home/pshao7/dvrk_python_devel/dataset/multi_array_exp.txt', skiprows=1)
+gamma = -0.6
+beta = 0.5
 
-m1_force = np.loadtxt('multi_m1_force_0605.txt', skiprows=1)
-m2_force = np.loadtxt('multi_m2_force_0605.txt', skiprows=1)
-puppet_force = np.loadtxt('multi_puppet_force_0605.txt', skiprows=1)
-total = 0.5 * (m1_force + m2_force)
+m1_force = np.loadtxt(f'/home/pshao7/dvrk_python_devel/dataset/multi_m1_force.txt', skiprows=1, usecols=(0, 1, 2))
+m2_force = np.loadtxt(f'/home/pshao7/dvrk_python_devel/dataset/multi_m2_force.txt', skiprows=1, usecols=(0, 1, 2))
+puppet_force = np.loadtxt(f'/home/pshao7/dvrk_python_devel/dataset/multi_puppet_force.txt', skiprows=1, usecols=(0, 1, 2))
+total_force = beta * m1_force + (1-beta) * m2_force
+puppet_force = gamma * puppet_force
+
+# # capture when contacting
+# true_pos = true_pos[750:]
+# expect_pos = expect_pos[750:]
+# total_force = total_force[750:]
+# puppet_force = puppet_force[750:]
 
 m1_force_norm = np.linalg.norm(m1_force, axis=1)
 m2_force_norm = np.linalg.norm(m2_force, axis=1)
 puppet_force_norm = np.linalg.norm(puppet_force, axis=1)
-num = len(true_pos)
+total_force_norm = beta * m1_force_norm + (1-beta) * m2_force_norm
+num = len(m1_force)
 x = np.arange(num) * 0.002
 
-# rmse = np.sqrt(np.mean((true_pos - expect_pos)**2))
+# # RMSE for pos
+# std_pos = np.std(true_pos, axis=0)
+# rmse_pos = np.sqrt(np.mean((true_pos - expect_pos)**2, axis=0))
+# print(f"rmse of pos: {rmse_pos*1000} mm")
+# print("")
+
+# Normalized-RMSE for force (divided by range)
+force_max = np.max(puppet_force, axis=0)
+force_min = np.min(puppet_force, axis=0)
+
+rmse_force = np.sqrt(np.mean((total_force - puppet_force)**2, axis=0))
+nrmse_force = rmse_force / (force_max - force_min)
+print(f"rmse_force: {rmse_force} N")
+print(f"nrmse of force: {nrmse_force*100} %")
 
 matplotlib.rcParams['axes.linewidth'] = 1.5
 lw = 2
 
-plt.figure()
-# plt.subplot(311)
-plt.plot(x, true_pos[:,0]*1000, linewidth=lw, label='true_pos_x')
-plt.plot(x, true_pos[:,1]*1000, linewidth=lw, label='true_pos_y')
-plt.plot(x, true_pos[:,2]*1000, linewidth=lw, label='true_pos_z')
-# plt.plot(expect_pos)
-plt.plot(x, expect_pos[:,0]*1000, linewidth=lw, label='expect_pos_x')
-plt.plot(x, expect_pos[:,1]*1000, linewidth=lw, label='expect_pos_y')
-plt.plot(x, expect_pos[:,2]*1000, linewidth=lw, label='expect_pos_z')
-plt.xticks(fontsize=14, fontweight='bold')
-plt.yticks(fontsize=14, fontweight='bold')
-plt.xlabel("Time (s)", fontsize=14, fontweight='bold')
-plt.ylabel("Position (mm)", fontsize=14, fontweight='bold')
-plt.title("Position tracking", fontsize=16, fontweight='bold')
-plt.legend()
-# plt.figure()
-# # # plt.subplot(312)
-# # plt.plot(x, error[:,0], linewidth=lw, label='error in x axis')
-# # plt.plot(x, error[:,1], linewidth=lw, label='error in y axis')
-# # plt.plot(x, error[:,2], linewidth=lw, label='error in z axis')
-# # plt.xticks(fontsize=14, fontweight='bold')
-# # plt.yticks(fontsize=14, fontweight='bold')
-# # plt.xlabel("Time (s)", fontsize=14, fontweight='bold')
-# # plt.ylabel("Position (mm)", fontsize=14, fontweight='bold')
-# # plt.title("Absolute position error", fontsize=16, fontweight='bold')
-# # plt.legend()
-# # plt.figure()
-# # plt.subplot(313)
-# plt.plot(x, total[:,2], linewidth=lw, label='total_force')
-# # plt.plot(x, m2_force[:,2], linewidth=lw, label='m2_force')
-# plt.plot(x, puppet_force[:,2], linewidth=lw, label='puppet_force')
-# plt.xticks(fontsize=14, fontweight='bold')
-# plt.yticks(fontsize=14, fontweight='bold')
-# plt.xlabel("Time (s)", fontsize=14, fontweight='bold')
-# plt.ylabel("Force (N)", fontsize=14, fontweight='bold')
-# plt.title("Force in Z-axis", fontsize=16, fontweight='bold')
-# plt.legend()
-# plt.tight_layout()
-# plt.show()
-# # plt.figure()
-# # # plt.subplot(224)
-# # plt.plot(m1_force_norm, label='m1_force')
-# # plt.plot(m2_force_norm, label='m2_force')
-# # plt.plot(puppet_force_norm, label='puppet_force')
-# # plt.legend()
-# # plt.show()
 
-
-
-
-fig, axs = plt.subplots(3, 1, figsize=(10, 12), sharex=True)
+# plot position
+fig, axs = plt.subplots(3, 1, figsize=(12, 14), sharex=True)
 
 # X-axis force
-axs[0].plot(x, total[:,0], linewidth=lw, label='m1+m2_force_x')
+axs[0].plot(x, true_pos[:,0]*1000, linewidth=lw, label='true_psm_x')
+axs[0].plot(x, expect_pos[:,0]*1000, linewidth=lw, label='expect_psm_x')
+axs[0].set_ylabel("Position X (mm)", fontsize=14, fontweight='bold')
+axs[0].set_title("Position in X-axis", fontsize=16, fontweight='bold')
+axs[0].legend()
+axs[0].tick_params(labelsize=14)
+
+# Y-axis force
+axs[1].plot(x, true_pos[:,1]*1000, linewidth=lw, label='true_psm_y')
+axs[1].plot(x, expect_pos[:,1]*1000, linewidth=lw, label='expect_psm_y')
+axs[1].set_ylabel("Position Y (mm)", fontsize=14, fontweight='bold')
+axs[1].set_title("Position in Y-axis", fontsize=16, fontweight='bold')
+axs[1].legend()
+axs[1].tick_params(labelsize=14)
+
+# Z-axis force
+axs[2].plot(x, true_pos[:,2]*1000, linewidth=lw, label='true_psm_z')
+axs[2].plot(x, expect_pos[:,2]*1000, linewidth=lw, label='expect_psm_z')
+axs[2].set_xlabel("Time (s)", fontsize=14, fontweight='bold')
+axs[2].set_ylabel("Position Z (mm)", fontsize=14, fontweight='bold')
+axs[2].set_title("Position in Z-axis", fontsize=16, fontweight='bold')
+axs[2].legend()
+axs[2].tick_params(labelsize=14)
+
+fig.tight_layout()
+
+
+
+# plot force
+fig, axs = plt.subplots(3, 1, figsize=(12, 14), sharex=True)
+
+# X-axis force
+axs[0].plot(x, total_force[:,0], linewidth=lw, label='m1+m2_force_x')
 axs[0].plot(x, puppet_force[:,0], linewidth=lw, label='puppet_force_x')
 axs[0].set_ylabel("Force X (N)", fontsize=14, fontweight='bold')
 axs[0].set_title("Force in X-axis", fontsize=16, fontweight='bold')
@@ -83,7 +90,7 @@ axs[0].legend()
 axs[0].tick_params(labelsize=14)
 
 # Y-axis force
-axs[1].plot(x, total[:,1], linewidth=lw, label='m1+m2_force_y')
+axs[1].plot(x, total_force[:,1], linewidth=lw, label='m1+m2_force_y')
 axs[1].plot(x, puppet_force[:,1], linewidth=lw, label='puppet_force_y')
 axs[1].set_ylabel("Force Y (N)", fontsize=14, fontweight='bold')
 axs[1].set_title("Force in Y-axis", fontsize=16, fontweight='bold')
@@ -91,7 +98,7 @@ axs[1].legend()
 axs[1].tick_params(labelsize=14)
 
 # Z-axis force
-axs[2].plot(x, total[:,2], linewidth=lw, label='m1+m2_force_z')
+axs[2].plot(x, total_force[:,2], linewidth=lw, label='m1+m2_force_z')
 axs[2].plot(x, puppet_force[:,2], linewidth=lw, label='puppet_force_z')
 axs[2].set_xlabel("Time (s)", fontsize=14, fontweight='bold')
 axs[2].set_ylabel("Force Z (N)", fontsize=14, fontweight='bold')
@@ -101,6 +108,21 @@ axs[2].tick_params(labelsize=14)
 
 fig.tight_layout()
 plt.show()
+
+
+
+# #  plot force norm
+# fig, axs = plt.subplots(1, 1, figsize=(12, 14), sharex=True)
+
+# axs.plot(x, total_force_norm, linewidth=lw, label='m1+m2_force_x')
+# axs.plot(x, puppet_force_norm, linewidth=lw, label='puppet_force_x')
+# axs.set_ylabel("Force (N)", fontsize=14, fontweight='bold')
+# axs.set_title("Force norm", fontsize=16, fontweight='bold')
+# axs.legend()
+# axs.tick_params(labelsize=14)
+
+# fig.tight_layout()
+# plt.show()
 
 
 
